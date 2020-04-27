253db8e7516e86c59d5cd3e93407f8a1cdf84f34
guilhermeseiti <gseitihomma@gmail.com>
2020-04-27 17:47:24 -0300

    change title card

--- src/Card/Title/Title.js
+++ src/Card/Title/Title.js
@@ -3,7 +3,7 @@ import React from 'react';
 import classes from './Title.module.css';
 
 const title = (props) => (
-    <div className={classes.Title}>Tanque {props.title}</div>
+    <div className={classes.Title}>Machine {props.title}</div>
 )
 
 export default title
